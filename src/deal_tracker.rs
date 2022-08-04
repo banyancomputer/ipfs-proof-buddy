723c09d406dc6254570f42b80ff0f15ad56d6663
Claudia Richoux <c@laudiacay.cool>
2022-08-04 21:04:34 +0200

    mitski hours: https://open.spotify.com/track/3sslYZcFKtUvIEWN9lADgr\?si\=f9796f77995e4e84

--- src/deal_tracker.rs
+++ src/deal_tracker.rs
@@ -1,40 +1,52 @@
 // DataBase? more like DaBaby! https://www.youtube.com/watch?v=mxFstYSbBmc
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use cid::Cid;
+use ethers::providers::{Middleware, Provider, Http};
+use ethers::types::RewardType::Block;
 use serde::de::Error;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use sled::IVec;
+use proof_gen::gen_proof;
+
+// TODO: ensure this is safe if it falls over in the middle of a transaction. you've done half a job...
 
 const SLED_FILE: &str = "deal_tracker.sled";
 const DEAL_DB_IDENT: &str = "deal_db";
 const SCHEDULE_DB_IDENT: &str = "schedule_db";
+const CURRENT_THROUGH_BLOCK_N_KEY: &str = "current_through_block_n";
 
 lazy_static::lazy_static! {
     static ref DB: ProofScheduleDb = {
         let db = sled::open(SLED_FILE).unwrap();
+        let deal_tree = typed_sled::Tree::open(&db, DEAL_DB_IDENT);
+        let schedule_tree = typed_sled::Tree::open(&db, SCHEDULE_DB_IDENT);
         ProofScheduleDb {
-            deal_tree: db.open_tree(DEAL_DB_IDENT).unwrap(),
-            schedule_tree: db.open_tree(SCHEDULE_DB_IDENT).unwrap(),
+            db,
+            deal_tree,
+            schedule_tree
         }
     };
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, PartialOrd, Ord)]
 struct DealID(u64);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, PartialOrd, Ord)]
+struct BlockNum(u64);
 
 #[derive(Serialize, Deserialize)]
 struct DealParams {
     on_chain_deal_info: OnChainDealInfo,
-    next_proof_window_start_timestamp: u64,
-    last_proof_submission_timestamp: u64,
-    last_proof_submission_blocknumber: u64,
+    next_proof_window_start_blocknumber: BlockNum,
+    last_proof_submission_blocknumber: BlockNum,
 }
 
 struct ProofScheduleDb {
+    db: sled::Db,
     /// on_chain deal id --mapped_to--> DealParams
-    deal_tree: sled::Tree,
+    deal_tree: typed_sled::Tree<DealID, DealParams>,
     /// window --mapped_to--> on_chain deal id
-    schedule_tree: sled::Tree,
+    schedule_tree: typed_sled::Tree<BlockNum, Vec<DealID>>,
 }
 
 fn serialize_cid<S: Serializer>(cid: &Cid, s: S) -> Result<S::Ok, S::Error> {
@@ -53,9 +65,9 @@ where
 
 #[derive(Serialize, Deserialize, Clone, Copy)]
 struct OnChainDealInfo {
-    deal_start_timestamp: u64,
-    deal_length: u64,
-    proof_frequency: u64,
+    deal_start_block: BlockNum,
+    deal_length_in_blocks: BlockNum,
+    proof_frequency_in_blocks: BlockNum,
     #[serde(serialize_with = "serialize_cid", deserialize_with = "deserialize_cid")]
     ipfs_file_cid: Cid,
     ipfs_file_size: u64,
@@ -81,6 +93,20 @@ impl From<IVec> for DealID {
 }
 
 impl ProofScheduleDb {
+    fn schedule(&self, blocknum: BlockNum, deal_id: DealID) -> Result<()> {
+        let _ = self.schedule_tree.fetch_and_update(
+            &blocknum,
+            |maybe_deal_ids| match maybe_deal_ids {
+                Some(deal_ids) => {
+                    let mut deal_ids = deal_ids.clone();
+                    deal_ids.push(deal_id);
+                    Some(deal_ids)
+                }
+                None => Some(vec![deal_id]),
+            },
+        )?;
+        Ok(())
+    }
     // TODO: claudia, you are a trainwreck, and so is this code. use me: https://github.com/spacejam/sled/blob/main/examples/structured.rs
     // relate the on-chain ID to the DealParams struct.
     fn add_a_deal_to_db(&self, deal_id: DealID, cid: Cid) -> Result<()> {
@@ -92,18 +118,52 @@ impl ProofScheduleDb {
         let deal_params = DealParams {
             on_chain_deal_info,
             // TODO: figure out what if we only hear about the deal really late?
-            next_proof_window_start_timestamp: on_chain_deal_info.deal_start_timestamp,
-            last_proof_submission_timestamp: 0,
-            last_proof_submission_blocknumber: 0,
+            next_proof_window_start_blocknumber: on_chain_deal_info.deal_start_block,
+            last_proof_submission_blocknumber: BlockNum(0),
         };
-        let deal_params_bytes = serde_json::to_string(&deal_params)?;
         self.deal_tree
-            .insert(deal_id.0.to_le_bytes(), deal_params_bytes.as_str())?;
+            .insert(&deal_id, &deal_params)?;
 
         // TODO: put into scheduler!
-        self.schedule_tree.insert(
-            deal_params.next_proof_window_start_timestamp.to_le_bytes(),
-            deal_id,
+        self.schedule(on_chain_deal_info.deal_start_block, deal_id)?;
+        Ok(())
+    }
+
+    // TODO: make DB stuff atomic i think
+    async fn wake_up(&self) -> Result<()> {
+        // what's the last block that we proved our deals up through?
+        let current_through_block_n = match self.db.get(CURRENT_THROUGH_BLOCK_N_KEY)? {
+            Some(current_through_block_n_vec) => {
+                let mut current_through_block_n_bytes = [0u8; 8];
+                current_through_block_n_bytes.copy_from_slice(&*current_through_block_n_vec);
+                BlockNum(u64::from_le_bytes(current_through_block_n_bytes))
+            },
+            None => BlockNum(0),
+        };
+
+        // TODO: lazystatic this provider
+        // construct an ethers provider
+        let provider = Provider::<Http>::try_from(
+            "https://mainnet.infura.io/v3/idk hee hee",
+        )?;
+        // call get_block_number() on the provider to get the current block number.
+        let current_block_n = BlockNum(provider.get_block_number().await?.as_u64());
+
+        // TODO: do the proofs, submit them, and move them around in the scheduler as needed.
+        for block_and_deals in self.schedule_tree.range(current_through_block_n..current_block_n) {
+            let (block, deal_ids) = block_and_deals?;
+            for deal_id in deal_ids.iter() {
+                let deal_params = self.deal_tree.get(&deal_id)?.ok_or(anyhow!("no deal params found for deal id {:?}", deal_id))?;
+                let proof_to_post = gen_proof(block, deal_params.on_chain_deal_info.ipfs_file_cid, deal_params.on_chain_deal_info.ipfs_file_size)?;
+                post_proof(deal_id, proof_to_post)?;
+            }
+            self.schedule_tree.remove(&block)?;
+        }
+
+        // update the last block we proved up to.
+        let _ = self.db.insert(
+            CURRENT_THROUGH_BLOCK_N_KEY,
+            &(current_block_n.0.to_le_bytes())
         )?;
         Ok(())
     }
